abbfbff3247d290766b5d22391b848de7871b6a1
DavidFlores2803 <60612206+DavidFlores2803@users.noreply.github.com>
2020-05-08 14:43:53 -0400

    Organised the folders
    Added the ps1 file

---
+++ PersonalWork/ModernSolutions.ps1
@@ -0,0 +1,51 @@
+﻿Param
+(
+    [string] $processName = "notepad"
+)
+
+Function KillThatProcess([string] $processName)
+{
+    $numOfProcesses = (Get-Process $processName).Count
+
+    $userInput = Read-Host("There are $numOfProcesses with the name $processName, proceed? Y/N")
+
+    if($userInput.ToLower() -eq "y")
+    {
+        Write-Host -ForegroundColor Magenta -BackgroundColor Yellow "Procedure Worked !"
+        Get-Process $processName | Stop-Process
+    }
+    else
+    {
+        Write-Host -ForegroundColor Magenta -BackgroundColor Yellow "Procedure did not work! No processes were stopped"
+    }
+
+}
+KillThatProcess($processName)
+Function Bamboozle
+{
+
+    Set-Location -Path C:\Users\david\OneDrive\Escritorio\ITasg3
+    $character = -join ((65..90) + (97..122) | Get-Random -Count 1 | % {[char]$_})
+    Write-Host -ForegroundColor Green -BackgroundColor Black "
+⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀           
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀          
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀          
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀          
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆         
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿        
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀          
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀         
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀         
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀         
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀          
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀        
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀         
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀          
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉                       
+THIS IS SHREK I JUST HACKED YOUR COMPUTER   
+DIRECTLY FROM MY SWAMP AND I DON'T LIKE THE 
+LETTER $character SO I'M GOING TO DELETE ALL
+THE FILES THAT CONTAIN THAT LETTER"
+    [string] $fileList = Get-ChildItem -Path C:\Users\david\OneDrive\Escritorio\* -Include *$character* |Remove-Item -WhatIf
+}
+Bamboozle
